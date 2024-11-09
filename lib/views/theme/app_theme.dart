3a492ee4173348ed1efcd8c10dc9ef6b925eb72b
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-11-09 11:59:02 +0100

    Add path_provider plugin registration and update dependencies in pubspec.yaml

---
+++ lib/views/theme/app_theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+    fontFamily: 'Satoshi',
+    textTheme: const TextTheme(bodyMedium: TextStyle(color: Color(0xff0D0C0C))),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
+    textTheme: const TextTheme(bodyMedium: TextStyle(color: Color(0xffffffff))),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+}
