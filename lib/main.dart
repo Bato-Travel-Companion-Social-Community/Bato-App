81a1fdc866e245c232c756d531c9b7967a4a0b3a
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-11-09 12:19:56 +0100

    Refactor MyApp to support dynamic theming and integrate AppTheme for light and dark modes

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
+import 'views/main.dart' show AppTheme;
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void toggleTheme(bool isDarkMode) {
+    setState(() {
+      _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
-        ),
-        body: const Center(
-          child: Text('Hello, World!'),
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: _themeMode,
+      home: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
         ),
       ),
     );
---
+++ lib/views/main.dart
@@ -0,0 +1,3 @@
+// theme
+export 'theme/app_colors.dart';
+export 'theme/app_theme.dart';
