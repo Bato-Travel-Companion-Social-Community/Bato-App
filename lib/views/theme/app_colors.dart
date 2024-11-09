d85d334a0fdd7d43c04b7e667a38bd526bd7cbbf
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-11-09 11:51:06 +0100

    Remove unnecessary comments in main.dart and add AppColors class for centralized color management

---
+++ lib/views/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff40E0D0);
+  static const lightBackground = Color(0xffffffff);
+  static const darkBackground = Color(0xff0D0C0C);
+  static const grey = Color(0xffBEBEBE);
+  static const darkGrey = Color(0xff343434);
+}
