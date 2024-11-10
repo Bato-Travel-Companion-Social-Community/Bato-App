e112a025e45796a1dc7f0650c2c7ab2e47f0d94b
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-11-10 20:20:40 +0100

    Documentation for Logo and AppTextStyles components with detailed comments and usage examples

--- lib/views/components/logo.dart
+++ lib/views/components/logo.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import '../../views/main.dart' show AppTextStyles, AppColors;
 
+/// A stateless widget that displays the logo text.
+///
+/// The [Logo] widget takes a [size] parameter which controls the font size of the text.
+/// The text displayed is 'Bato' with a bold font weight and a primary color defined in [AppColors].
+///
+/// Example usage:
+/// ```dart
+/// Logo(size: 24.0);
+/// ```
 class Logo extends StatelessWidget {
+  /// The size of the font for the logo text.
   final double size;
 
-  /// The [size] parameter controls the font size of the text.
-
-  const Logo({Key? key, required this.size}) : super(key: key);
+  /// Creates a [Logo] widget.
+  ///
+  /// The [size] parameter must not be null and is required to set the font size of the text.
+  const Logo({super.key, required this.size});
 
   @override
   Widget build(BuildContext context) {
