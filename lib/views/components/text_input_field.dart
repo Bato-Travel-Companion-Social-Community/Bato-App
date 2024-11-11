a2045272051cd2641d4c5e138809aeaebae5c2c8
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-11-11 01:35:31 +0100

    Refactor TextInputField to StatefulWidget and add password visibility toggle

--- lib/views/components/text_input_field.dart
+++ lib/views/components/text_input_field.dart
@@ -1,32 +1,69 @@
 import 'package:flutter/material.dart';
 import '../../views/main.dart' show AppColors;
 
-class TextInputField extends StatelessWidget {
+class TextInputField extends StatefulWidget {
   final String errorText;
   final String hintText;
   final bool isPassword;
 
-  const TextInputField(
-      {super.key,
-      required this.errorText,
-      required this.hintText,
-      this.isPassword = false});
+  const TextInputField({
+    Key? key,
+    required this.errorText,
+    required this.hintText,
+    this.isPassword = false,
+  }) : super(key: key);
+
+  @override
+  TextInputFieldState createState() => TextInputFieldState();
+}
+
+class TextInputFieldState extends State<TextInputField> {
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: TextStyle(color: AppColors.grey),
-          border: OutlineInputBorder(
-              borderSide: BorderSide(color: AppColors.grey),
-              borderRadius: BorderRadius.circular(10),
-              gapPadding: 2.0),
-          contentPadding: const EdgeInsets.all(8.0)),
-      obscureText: isPassword,
+        hintText: widget.hintText,
+        hintStyle: const TextStyle(color: AppColors.grey),
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        contentPadding: const EdgeInsets.all(8.0),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off : Icons.visibility,
+                  color: AppColors.grey,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              )
+            : null,
+      ),
+      cursorColor: AppColors.grey, // Set the cursor color to keep it consistent
+      obscureText: widget.isPassword ? _obscureText : false,
       validator: (String? value) {
         if (value == null || value.isEmpty) {
-          return errorText;
+          return widget.errorText;
         }
         return null;
       },
