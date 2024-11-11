fd41e4fcd832d9b0158503f15f3f6f8809a0e390
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-11-11 01:16:50 +0100

    Add LoginPage component with form validation and integrate it as the home widget

--- lib/views/main.dart
+++ lib/views/main.dart
@@ -5,6 +5,8 @@ export 'theme/app_text_styles.dart';
 
 // components
 export 'components/logo.dart';
+export 'components/text_input_field.dart';
 
 // pages
 export 'pages/splash.dart';
+export 'pages/login.dart';
---
+++ lib/views/pages/login.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../../views/main.dart' show Logo, TextInputField;
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(60.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Logo(size: 100),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextInputField(
+                        hintText: 'Email',
+                        errorText: 'Please enter your email',
+                      ),
+                      const SizedBox(height: 5),
+                      TextInputField(
+                        hintText: 'Password',
+                        errorText: 'Please enter your password',
+                        isPassword: true,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Processing Data'),
+                              ),
+                            );
+                          }
+                        },
+                        child: const Text('Submit'),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
