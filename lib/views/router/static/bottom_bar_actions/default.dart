c81235d79ee477127e056f480962b871e77a7a3f
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-11-20 18:42:37 +0100

    Remove unused FontAwesome import from default bottom bar actions

--- lib/views/router/static/bottom_bar_actions/default.dart
+++ lib/views/router/static/bottom_bar_actions/default.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../views/main.dart' show AppColors;
 
 List<Widget> getDefaultBottomBarActions(BuildContext context, int pageIndex) {
