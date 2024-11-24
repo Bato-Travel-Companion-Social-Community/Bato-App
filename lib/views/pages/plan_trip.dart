c4e7fe0cdd9050377fb7df7df97f1a6e5be566c1
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-11-24 21:36:50 +0100

    Add OneWayFlightCard and HotelCard widgets to PlanTripPage for flight and hotel listings

--- lib/views/pages/plan_trip.dart
+++ lib/views/pages/plan_trip.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../../views/main.dart' show AppColors;
+import '../../views/main.dart' show AppColors, OneWayFlightCard, HotelCard;
 
 class PlanTripPage extends StatelessWidget {
   const PlanTripPage({super.key});
@@ -18,15 +18,15 @@ class PlanTripPage extends StatelessWidget {
               FaIcon(FontAwesomeIcons.hotel),
             ],
             splashFactory: NoSplash.splashFactory,
-            overlayColor: MaterialStateProperty.resolveWith<Color?>(
-              (Set<MaterialState> states) {
-                return states.contains(MaterialState.focused)
+            overlayColor: WidgetStateProperty.resolveWith<Color?>(
+              (Set<WidgetState> states) {
+                return states.contains(WidgetState.focused)
                     ? null
                     : Colors.transparent;
               },
             ),
             labelColor: Theme.of(context).primaryColor,
-            labelPadding: EdgeInsets.all(10),
+            labelPadding: EdgeInsets.all(12),
             unselectedLabelColor: Colors.grey,
             indicatorColor: AppColors.primary,
           ),
@@ -34,8 +34,158 @@ class PlanTripPage extends StatelessWidget {
           Expanded(
             child: TabBarView(
               children: [
-                Center(child: Text('Content of Tab 1')),
-                Center(child: Text('Content of Tab 2')),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: ListView(
+                    children: [
+                      OneWayFlightCard(
+                        origin: 'Madrid',
+                        destination: 'Lisbon',
+                        departureDate: '2022-12-25',
+                        price: '200',
+                        airlineName: 'Delta',
+                        flightNumber: 'DL123',
+                        imageUrl:
+                            'https://th.bing.com/th?id=OSK.tBkF8a0UmuKhX8VZ-8hMA_AnGTguUv0CReaLS6zKd0c&w=46&h=46&c=11&rs=1&qlt=80&o=6&dpr=2&pid=SANGAM',
+                      ),
+                      OneWayFlightCard(
+                        origin: 'Madrid',
+                        destination: 'Lisbon',
+                        departureDate: '2022-12-25',
+                        price: '200',
+                        airlineName: 'Delta',
+                        flightNumber: 'DL123',
+                        imageUrl:
+                            'https://th.bing.com/th?id=OSK.tBkF8a0UmuKhX8VZ-8hMA_AnGTguUv0CReaLS6zKd0c&w=46&h=46&c=11&rs=1&qlt=80&o=6&dpr=2&pid=SANGAM',
+                      ),
+                      OneWayFlightCard(
+                        origin: 'Madrid',
+                        destination: 'Lisbon',
+                        departureDate: '2022-12-25',
+                        price: '200',
+                        airlineName: 'Delta',
+                        flightNumber: 'DL123',
+                        imageUrl:
+                            'https://th.bing.com/th?id=OSK.tBkF8a0UmuKhX8VZ-8hMA_AnGTguUv0CReaLS6zKd0c&w=46&h=46&c=11&rs=1&qlt=80&o=6&dpr=2&pid=SANGAM',
+                      ),
+                      OneWayFlightCard(
+                        origin: 'Madrid',
+                        destination: 'Lisbon',
+                        departureDate: '2022-12-25',
+                        price: '200',
+                        airlineName: 'Delta',
+                        flightNumber: 'DL123',
+                        imageUrl:
+                            'https://th.bing.com/th?id=OSK.tBkF8a0UmuKhX8VZ-8hMA_AnGTguUv0CReaLS6zKd0c&w=46&h=46&c=11&rs=1&qlt=80&o=6&dpr=2&pid=SANGAM',
+                      ),
+                      OneWayFlightCard(
+                        origin: 'Madrid',
+                        destination: 'Lisbon',
+                        departureDate: '2022-12-25',
+                        price: '200',
+                        airlineName: 'Delta',
+                        flightNumber: 'DL123',
+                        imageUrl:
+                            'https://th.bing.com/th?id=OSK.tBkF8a0UmuKhX8VZ-8hMA_AnGTguUv0CReaLS6zKd0c&w=46&h=46&c=11&rs=1&qlt=80&o=6&dpr=2&pid=SANGAM',
+                      ),
+                      OneWayFlightCard(
+                        origin: 'Madrid',
+                        destination: 'Lisbon',
+                        departureDate: '2022-12-25',
+                        price: '200',
+                        airlineName: 'Delta',
+                        flightNumber: 'DL123',
+                        imageUrl:
+                            'https://th.bing.com/th?id=OSK.tBkF8a0UmuKhX8VZ-8hMA_AnGTguUv0CReaLS6zKd0c&w=46&h=46&c=11&rs=1&qlt=80&o=6&dpr=2&pid=SANGAM',
+                      ),
+                      OneWayFlightCard(
+                        origin: 'Madrid',
+                        destination: 'Lisbon',
+                        departureDate: '2022-12-25',
+                        price: '200',
+                        airlineName: 'Delta',
+                        flightNumber: 'DL123',
+                        imageUrl:
+                            'https://th.bing.com/th?id=OSK.tBkF8a0UmuKhX8VZ-8hMA_AnGTguUv0CReaLS6zKd0c&w=46&h=46&c=11&rs=1&qlt=80&o=6&dpr=2&pid=SANGAM',
+                      ),
+                      OneWayFlightCard(
+                        origin: 'Madrid',
+                        destination: 'Lisbon',
+                        departureDate: '2022-12-25',
+                        price: '200',
+                        airlineName: 'Delta',
+                        flightNumber: 'DL123',
+                        imageUrl:
+                            'https://th.bing.com/th?id=OSK.tBkF8a0UmuKhX8VZ-8hMA_AnGTguUv0CReaLS6zKd0c&w=46&h=46&c=11&rs=1&qlt=80&o=6&dpr=2&pid=SANGAM',
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 16,
+                  ),
+                  child: ListView(
+                    children: [
+                      HotelCard(
+                          hotelName: 'Casablato',
+                          hotelLocation: 'Morocco',
+                          pricePerNight: '200',
+                          rating: '4.5',
+                          imageUrl:
+                              'https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                          reviewCount: '200',
+                          onBookNowPressed: () {}),
+                      HotelCard(
+                          hotelName: 'Casablato',
+                          hotelLocation: 'Morocco',
+                          pricePerNight: '200',
+                          rating: '4.5',
+                          imageUrl:
+                              'https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                          reviewCount: '200',
+                          onBookNowPressed: () {}),
+                      HotelCard(
+                          hotelName: 'Casablato',
+                          hotelLocation: 'Morocco',
+                          pricePerNight: '200',
+                          rating: '4.5',
+                          imageUrl:
+                              'https://images.pexels.com/photos/262047/pexels-photo-262047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                          reviewCount: '200',
+                          onBookNowPressed: () {}),
+                      HotelCard(
+                          hotelName: 'Casablato',
+                          hotelLocation: 'Morocco',
+                          pricePerNight: '200',
+                          rating: '4.5',
+                          imageUrl:
+                              'https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                          reviewCount: '200',
+                          onBookNowPressed: () {}),
+                      HotelCard(
+                          hotelName: 'Casablato',
+                          hotelLocation: 'Morocco',
+                          pricePerNight: '200',
+                          rating: '4.5',
+                          imageUrl:
+                              'https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                          reviewCount: '200',
+                          onBookNowPressed: () {}),
+                      HotelCard(
+                          hotelName: 'Casablato',
+                          hotelLocation: 'Morocco',
+                          pricePerNight: '200',
+                          rating: '4.5',
+                          imageUrl:
+                              'https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                          reviewCount: '200',
+                          onBookNowPressed: () {}),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
